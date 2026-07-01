25b4f5b02206abe68eaf401261fb5139c4d473df
Instancian <Instancian@instancium.com>
2026-07-01 11:13:30 +0300

    chore(gitignore): remove config folder

---
+++ src/InstantAIGate.Application/Config/GatewayConfig.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InstantAIGate.Application.Config
+{
+    public class GatewayConfig
+    {
+        public string RootPath { get; set; } = Path.Combine(
+            Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData),
+            "InstantAIGate"
+        );
+
+        public const string AdminKeyFileName = "admin.key";
+        public string AdminKeyPath => Path.Combine(RootPath, AdminKeyFileName);
+
+        public string? AdminKey { get; set; }
+    }
+}
