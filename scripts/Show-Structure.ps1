6df1ed370607a54ac88ce265bf4d58f764c3c869
Instancian <Instancian@instancium.com>
2026-09-14 16:02:30 +0300

    change gitignore

--- scripts/Show-Structure.ps1
+++
@@ -1,3 +0,0 @@
-Get-ChildItem -Recurse -Directory -Include bin, obj, wwwroot | ForEach-Object { $_.Attributes = 'Hidden' }
-tree /F
-Get-ChildItem -Recurse -Directory -Include bin, obj, wwwroot -Force | ForEach-Object { $_.Attributes = 'Normal' }
